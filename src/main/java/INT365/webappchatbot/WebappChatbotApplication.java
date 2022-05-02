76ab9b798d6935ea4285b3ea3726d1c98fab1f23
Sahachai Senarak <sahachai.kmutt@mail.kmutt.ac.th>
2022-05-03 03:01:33 +0700

    [Project Initialzation] : [initial] the project

---
+++ src/main/java/INT365/webappchatbot/WebappChatbotApplication.java
@@ -0,0 +1,13 @@
+package INT365.webappchatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebappChatbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebappChatbotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/INT365/webappchatbot/WebappChatbotApplicationTests.java
@@ -0,0 +1,13 @@
+package INT365.webappchatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebappChatbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
