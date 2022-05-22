4d4d8c4d1f52f4943f866ff4968faceeda5b441e
Sahachai Senarak <sahachai.kmutt@mail.kmutt.ac.th>
2022-05-23 02:37:25 +0700

    [Adding] : add cross origin in controller

--- src/main/java/INT365/webappchatbot/Controllers/ChatController.java
+++ src/main/java/INT365/webappchatbot/Controllers/ChatController.java
@@ -7,7 +7,9 @@ import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:3000")
 @Controller
 public class ChatController {
 
