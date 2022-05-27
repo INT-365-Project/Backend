e785839fc2145765b4d09b3824a3075c19f5c64d
Sahachai Senarak <sahachai.kmutt@mail.kmutt.ac.th>
2022-05-28 03:20:23 +0700

    [Reformatting] : reformat file controller

--- src/main/java/INT365/webappchatbot/Controllers/FileController.java
+++ src/main/java/INT365/webappchatbot/Controllers/FileController.java
@@ -14,7 +14,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestController
-@CrossOrigin(value = "http://20.92.229.38",allowedHeaders = "*")
+@CrossOrigin(value = "http://20.92.229.38", allowedHeaders = "*")
 public class FileController {
 
     @Autowired
