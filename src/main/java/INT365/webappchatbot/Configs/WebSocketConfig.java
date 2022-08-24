d57aad48f2089ce9c8dcc14db5979abbeefd1216
Sahachai Senarak <sahachai.kmutt@mail.kmutt.ac.th>
2022-08-24 15:58:33 +0700

    [Update] - update api chat path

--- src/main/java/INT365/webappchatbot/Configs/WebSocketConfig.java
+++ src/main/java/INT365/webappchatbot/Configs/WebSocketConfig.java
@@ -19,6 +19,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry){
-        registry.addEndpoint("/chat").setAllowedOrigins("*").withSockJS();
+        registry.addEndpoint("/api/chat").setAllowedOrigins("*").withSockJS();
     }
 }
