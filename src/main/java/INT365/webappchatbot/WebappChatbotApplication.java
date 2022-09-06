ec4f75b082213598bd48c57c2494c56751030b3a
Sahachai Senarak <sahachai.kmutt@mail.kmutt.ac.th>
2022-09-06 17:43:46 +0700

    [Test] - add @EnableAsync to enable async

--- src/main/java/INT365/webappchatbot/WebappChatbotApplication.java
+++ src/main/java/INT365/webappchatbot/WebappChatbotApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -11,6 +12,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 @SpringBootApplication
 @EnableFeignClients
+@EnableAsync
 public class WebappChatbotApplication {
 
     public static void main(String[] args) {
