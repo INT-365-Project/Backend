ae02ce2e14074b753e21ada0a0ff2db9f68bb5ff
Sahachai Senarak <sahachai.kmutt@mail.kmutt.ac.th>
2022-09-12 01:12:40 +0700

    [Chat] : [Update] - update web socket endpoint

--- src/main/java/INT365/webappchatbot/Configs/WebSocketConfig.java
+++ src/main/java/INT365/webappchatbot/Configs/WebSocketConfig.java
@@ -13,7 +13,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry){
         registry.setApplicationDestinationPrefixes("/app");
-        registry.enableSimpleBroker("/chatroom","/user");
+        registry.enableSimpleBroker("/chatroom","/user","/updateNewChat");
         registry.setUserDestinationPrefix("/user");
     }
 
