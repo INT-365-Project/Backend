626dee7b846bc527c8de59082931e227c27f872a
Sahachai Senarak <sahachai.kmutt@mail.kmutt.ac.th>
2022-11-16 16:30:12 +0700

    [Chat] - test

--- src/main/java/INT365/webappchatbot/Controllers/ChatController.java
+++ src/main/java/INT365/webappchatbot/Controllers/ChatController.java
@@ -42,7 +42,7 @@ public class ChatController {
             this.chatService.setMessageRead(message);
         }
         if (message.getStatus().equals(Status.MESSAGE)) {
-//            simpMessagingTemplate.convertAndSendToUser(message.getReceiverName(), "/private", message); // path > /user/{receiverName}/private
+            simpMessagingTemplate.convertAndSendToUser(message.getReceiverName(), "/private", message); // path > /user/{receiverName}/private
             return this.chatService.saveChat(message);
         }
         if (message.getType().equals(WebhookMessageType.IMAGE.getType())) {
